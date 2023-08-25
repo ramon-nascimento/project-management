8fb4b283477184a0a9e6dac937bac4846ffc978f
Ramon Nascimento <rnq.filho@hotmail.com>
2023-08-25 00:33:01 -0300

    [prisma] configuração da ORM e do banco de dados.

---
+++ prisma/migrations/20230825005305_initial_migration/migration.sql
@@ -0,0 +1,16 @@
+-- CreateTable
+CREATE TABLE "Project" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "title" TEXT NOT NULL,
+    "description" TEXT NOT NULL
+);
+
+-- CreateTable
+CREATE TABLE "Task" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "title" TEXT NOT NULL,
+    "description" TEXT,
+    "status" TEXT,
+    "projectId" TEXT NOT NULL,
+    CONSTRAINT "Task_projectId_fkey" FOREIGN KEY ("projectId") REFERENCES "Project" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
